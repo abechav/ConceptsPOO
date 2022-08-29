4ef59fd39a1bfb62f704288ec550fe0ad1af2e26
Alejandro Betancur Echavarria <abechav@bancolombia.com.co>
2022-08-29 18:31:07 -0500

    Part 1 - Date Class

---
+++ ConceptsPOO/ConceptsPOO/Date.cs
@@ -0,0 +1,66 @@
+﻿using POOConcepts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = month;
+            _day = CheckDay(year, month, day);
+        }
+
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+
+            throw new MonthException("Invalid Month");
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            try
+            {
+                if (month == 2 && month <= 29 && IsLeapYear(year))
+                {
+                    return day;
+                }
+
+                int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                if (day >= 1 && day <= daysPerMonth[month])
+                {
+                    return day;
+                }
+
+                throw new DayException("Invalid Day");
+            }
+            catch (Exception)
+            {
+                throw new DayException("Invalid Day");
+            }
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{_year}/{CheckMonth(_month):00}/{_day:00}";
+        }
+    }
+}
---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConceptsPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+                Date date1 = new Date(2022, 8, 9);
+                Console.WriteLine(date1);
+                Console.WriteLine(new Date(2022, 18, 19));
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
